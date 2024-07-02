cc55623d419edcfbec0a58689cc0c4746676ee5f
Spider-999 <67486366+Spider-999@users.noreply.github.com>
2024-07-02 16:59:42 +0300

    Add files via upload

---
+++ wire_chat/client.py
@@ -0,0 +1,132 @@
+'''
+Copyright (C) 19/06/2024 Hary Patrascu
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+Last updated: 19/06/2024
+Contact: email: harypatrascu.1@gmail.com
+'''
+import socket
+import threading
+from gui import App, tk, ctk, filedialog
+
+
+class Client(App):
+    def __init__(self, window):
+        super().__init__(window)
+        # Create the graphical user interface object
+        # The client class inherits from the app class
+        self.chat.text_entry.bind('<Return>', self.send_message)
+        self.login.username.bind('<Return>', self.get_username)
+        self.chat.send_button.configure(command=self.send_message)
+        self.login.connect_button.configure(command=self.get_username)
+        self.chat.browse_button.configure(command=self.open_files)
+
+        # Sockets setup
+        self.HOST = '127.0.0.1'
+        self.PORT = 50000
+        self.BYTES = 4096
+        self.FORMAT = 'utf-8'
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.HOST, self.PORT))
+
+
+    def send_message(self, event=None):
+        # We need the event argument because binding the return key passed arguments to our send_message function
+        # Send message to the server to be broadcast to every client
+        try:
+            # Get the message from the Entry widget
+            message = self.chat.entry_message.get()
+
+            if message != '':
+                # Insert the text in the chat list and reset the entry string to an empty string
+                self.insert_text(f'you:{message}', self.chat.message_list)
+                self.chat.entry_message.set('')
+
+                # Encode it and send it to the server
+                encoded_message = message.encode('utf-8')
+                self.client_socket.sendall(encoded_message)
+        except socket.error as error:
+            print(f'[SEND_MESSAGE] {error}')
+
+
+    def receive_message(self):
+        # Receive messages from the server
+        usernames = ''
+        while True:
+            try:
+                message = self.client_socket.recv(self.BYTES).decode(self.FORMAT)
+
+                if message.startswith('NEW_CONNECTION:'):
+                    # Check for new usernames to add to the list
+                    usernames = message.split(':')[1].split(',')
+                    print(usernames)
+                    self.update_connections_list(usernames)
+                elif message:
+                    self.insert_text(message, self.chat.message_list)
+            except socket.error as error:
+                # If an error occurred print it
+                print(f'[RECEIVE_MESSAGE] {error}')
+                break
+        self.update_connections_list(usernames)
+        self.insert_text('[SERVER] - Connection lost.', self.chat.message_list)
+        self.client_socket.close()
+
+
+    def update_connections_list(self, usernames):
+        # Update the connections list
+        self.chat.connections_list.configure(state = 'normal')
+        self.chat.connections_list.delete('1.0', tk.END)
+        for user in usernames:
+            self.chat.connections_list.insert(tk.END, user + '\n')
+        self.chat.connections_list.configure(state = 'disabled')
+
+
+    def insert_text(self, message, widget):
+        # Insert text in the chat
+        widget.configure(state = 'normal')
+        widget.insert(tk.END, message + '\n')
+        widget.configure(state = 'disabled')
+
+
+    def get_username(self, event=None):
+        # Get the username and change the frame to the chat frame
+        username = self.login.entry_user.get()
+        if username != '':
+            self.client_socket.send(username.encode(self.FORMAT))
+            self.goto_chat()
+
+
+    def open_files(self):
+            # Open a file and send it to the chat
+            filename = filedialog.askopenfile(initialdir='/home/device3', title='Open file')
+            print(filename.name)
+            with open(filename.name, 'rb') as file:
+                try:
+                    data = file.read(self.BYTES)
+                    while data:
+                        message = 'FTP:'.encode(self.FORMAT) + data
+                        self.client_socket.send(message)
+                        data = file.read(self.BYTES)
+                except IOError as error:
+                    print('Invalid file name ' + error)
+
+    def start_client(self):
+        # Start the client thread
+        receive_thread = threading.Thread(target=self.receive_message)
+        receive_thread.start()
+
+
+window = ctk.CTk()
+client = Client(window)
+client.start_client()
+window.mainloop()
---
+++ wire_chat/server.py
@@ -0,0 +1,120 @@
+'''
+Copyright (C) 19/06/2024 Hary Patrascu
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+Last updated: 19/06/2024
+Contact: email: harypatrascu.1@gmail.com
+'''
+import socket
+import threading
+
+
+class Server:
+    def __init__(self):
+        # Setup variables
+        self.HOST = '127.0.0.1'
+        self.PORT = 50000
+        self.BYTES = 4096
+        self.FORMAT = 'utf-8'
+        self.MAX_CONNECTIONS = 10
+
+        # Setting the socket options to allow for reconnection and creating the server socket
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.HOST, self.PORT))
+
+        # Variables to store new clients
+        self.clients = []
+        self.usernames = []
+
+    def handle_client(self, client):
+        while True:
+            try:
+                message = client.recv(self.BYTES).decode(self.FORMAT)
+                # File transfer implementation on the client side
+                if message.startswith('FTP:'):
+                    filename = 'output.txt'
+                    message = message.split(':')[1]
+                    print(message, file=file)
+                    with open(filename, 'w') as file:
+                        while message:
+                            if not message:
+                                break
+                            else:
+                                print(message, file=file)
+                                message = client.recv(self.BYTES).split(':')[1].decode(self.FORMAT)
+                elif message != '':
+                    print(f'{self.usernames[self.clients.index(client)]}:{message}')
+                    self.broadcast(f'{self.usernames[self.clients.index(client)]}:{message}', client)
+            except socket.error as error:
+                print(error)
+
+                # Remove username and client from the arrays
+                user_index = self.clients.index(client)
+                self.clients.remove(client)
+
+                username = self.usernames[user_index]
+                self.usernames.remove(username)
+
+                # Update the connections list when someone disconnects
+                self.broadcast_connections()
+
+                print(f'[SERVER] {username} disconnected.')
+                self.broadcast(f'[SERVER] {username} disconnected.', client)
+                break
+        client.close()
+
+    def broadcast(self, message, this_client):
+        for client in self.clients:
+            if client != this_client:
+                client.sendall(message.encode(self.FORMAT))
+
+
+    def broadcast_connections(self):
+        message = f'NEW_CONNECTION:{','.join(self.usernames)}'
+        for client in self.clients:
+            client.sendall(message.encode(self.FORMAT))
+
+    def receive(self):
+        # Listen for maximum 10 new connections
+        self.server_socket.listen(self.MAX_CONNECTIONS)
+        while True:
+            # Accept a new connection
+            client, address = self.server_socket.accept()
+            print(f'[SERVER] New connection from {address}')
+
+            # Get the username of the new client
+            username = client.recv(self.BYTES).decode(self.FORMAT)
+
+            # Append the new variables to the storage lists
+            self.usernames.append(username)
+            self.clients.append(client)
+
+            # Send the username list to the client to be appended to the active connections list
+            self.broadcast_connections()
+
+            # Let everyone know someone joined and show the new client that the connection was successful
+            print(f'[SERVER] {username} joined the chat.')
+            self.broadcast(f'{self.usernames[self.clients.index(client)]} joined the chat.', client)
+            client.sendall('[SERVER] Connected.'.encode(self.FORMAT))
+
+
+            # Start a server thread
+            server_thread = threading.Thread(target=self.handle_client, args=(client,))
+            server_thread.start()
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.receive()
+    server.server_socket.close()
